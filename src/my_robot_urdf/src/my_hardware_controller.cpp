cdeb58da0381e425c7fddf1e458fdb287bcb90bb
andy <andy@andy-box>
2025-12-08 15:59:27 +0800

    update to jammy

--- src/my_robot_urdf/src/my_hardware_controller.cpp
+++ src/my_robot_urdf/src/my_hardware_controller.cpp
@@ -1,19 +1,13 @@
-// Copyright 2023 ros2_control Development Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/macros.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
 #include <chrono>
 #include <cmath>
 #include <iomanip>
@@ -21,13 +15,6 @@
 #include <memory>
 #include <sstream>
 #include <vector>
-#include "hardware_interface/handle.hpp"
-#include "hardware_interface/hardware_info.hpp"
-#include "hardware_interface/system_interface.hpp"
-#include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "rclcpp/macros.hpp"
-#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
-#include "rclcpp_lifecycle/state.hpp"
 
 namespace my_robot_urdf
 {
@@ -35,195 +22,215 @@ namespace my_robot_urdf
 class MyHardwareController : public hardware_interface::SystemInterface
 {
   public:
-    hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareComponentInterfaceParams &params) override
+    RCLCPP_SHARED_PTR_DEFINITIONS(MyHardwareController)
+
+    hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override
     {
-        if (hardware_interface::SystemInterface::on_init(params) != hardware_interface::CallbackReturn::SUCCESS)
+        if (hardware_interface::SystemInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS)
         {
             return hardware_interface::CallbackReturn::ERROR;
         }
+        logger_ = std::make_shared<rclcpp::Logger>(rclcpp::get_logger("MyHardwareController"));
+        clock_ = std::make_shared<rclcpp::Clock>(rclcpp::Clock());
+
+        // info_ 是 info 的内部存储变量. 通过 SystemInterface::on_init 赋值
+        hw_states_positions_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        hw_states_velocities_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        hw_commands_positions_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        hw_commands_velocities_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+        control_level_.resize(info_.joints.size(), integration_level_t::POSITION);
+
+        RCLCPP_DEBUG(get_logger(), "on_init, info_.joints.size() = %d", info_.joints.size());
 
         return hardware_interface::CallbackReturn::SUCCESS;
     }
 
-    hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & /*previous_state*/) override
+    std::vector<hardware_interface::StateInterface> export_state_interfaces() override
     {
-        RCLCPP_INFO(logger_, "Configuring ...please wait...");
-
-        // 配置时重置所有值
-        for (const auto &[name, descr] : joint_state_interfaces_)
+        std::vector<hardware_interface::StateInterface> state_interfaces;
+        for (std::size_t i = 0; i < info_.joints.size(); i++)
         {
-            set_state(name, 0.0);
-        }
-        for (const auto &[name, descr] : joint_command_interfaces_)
-        {
-            set_command(name, 0.0);
-        }
-        for (const auto &[name, descr] : gpio_state_interfaces_)
-        {
-            set_state(name, 0.0);
-        }
-        for (const auto &[name, descr] : gpio_command_interfaces_)
-        {
-            set_command(name, 0.0);
+            RCLCPP_DEBUG(get_logger(), "export_state_interfaces, export state: %d, %s", i, info_.joints[i].name.c_str());
+            state_interfaces.emplace_back(hardware_interface::StateInterface(info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_positions_[i]));
+            state_interfaces.emplace_back(hardware_interface::StateInterface(info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_states_velocities_[i]));
         }
 
-        RCLCPP_INFO(logger_, "Successfully configured!");
+        return state_interfaces;
+    }
 
+    std::vector<hardware_interface::CommandInterface> export_command_interfaces() override
+    {
+        std::vector<hardware_interface::CommandInterface> command_interfaces;
+        for (std::size_t i = 0; i < info_.joints.size(); i++)
+        {
+            RCLCPP_DEBUG(get_logger(), "export_command_interfaces, export command: %d, %s", i, info_.joints[i].name.c_str());
+            command_interfaces.emplace_back(hardware_interface::CommandInterface(info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_positions_[i]));
+            command_interfaces.emplace_back(hardware_interface::CommandInterface(info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_commands_velocities_[i]));
+        }
+
+        return command_interfaces;
+    }
+
+    hardware_interface::return_type prepare_command_mode_switch(const std::vector<std::string> &start_interfaces, const std::vector<std::string> &stop_interfaces) override
+    {
+        // Prepare for new command modes
+        std::vector<integration_level_t> new_modes = {};
+        for (std::string key : start_interfaces)
+        {
+            for (std::size_t i = 0; i < info_.joints.size(); i++)
+            {
+                if (key == info_.joints[i].name + "/" + hardware_interface::HW_IF_POSITION)
+                {
+                    new_modes.push_back(integration_level_t::POSITION);
+                }
+                if (key == info_.joints[i].name + "/" + hardware_interface::HW_IF_VELOCITY)
+                {
+                    new_modes.push_back(integration_level_t::VELOCITY);
+                }
+                if (key == info_.joints[i].name + "/" + hardware_interface::HW_IF_ACCELERATION)
+                {
+                    new_modes.push_back(integration_level_t::ACCELERATION);
+                }
+            }
+        }
+        // Example criteria: All joints must be given new command mode at the same time
+        if (new_modes.size() != info_.joints.size())
+        {
+            return hardware_interface::return_type::ERROR;
+        }
+        // Example criteria: All joints must have the same command mode
+        if (!std::all_of(new_modes.begin() + 1, new_modes.end(), [&](integration_level_t mode) { return mode == new_modes[0]; }))
+        {
+            return hardware_interface::return_type::ERROR;
+        }
+
+        // Stop motion on all relevant joints that are stopping
+        for (std::string key : stop_interfaces)
+        {
+            for (std::size_t i = 0; i < info_.joints.size(); i++)
+            {
+                if (key.find(info_.joints[i].name) != std::string::npos)
+                {
+                    hw_commands_velocities_[i] = 0;
+                    control_level_[i] = integration_level_t::UNDEFINED; // Revert to undefined
+                }
+            }
+        }
+        // Set the new command modes
+        for (std::size_t i = 0; i < info_.joints.size(); i++)
+        {
+            if (control_level_[i] != integration_level_t::UNDEFINED)
+            {
+                // Something else is using the joint! Abort!
+                return hardware_interface::return_type::ERROR;
+            }
+            control_level_[i] = new_modes[i];
+        }
+        return hardware_interface::return_type::OK;
+    }
+
+    hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override
+    {
+        RCLCPP_DEBUG(get_logger(), "on_activate ...");
+
+        // Set some default values
+        for (std::size_t i = 0; i < hw_states_positions_.size(); i++)
+        {
+            hw_states_positions_[i] = 0;
+            hw_states_velocities_[i] = 0;
+        }
+        for (std::size_t i = 0; i < hw_commands_positions_.size(); i++)
+        {
+            hw_commands_positions_[i] = 0;
+            hw_commands_velocities_[i] = 0;
+            control_level_[i] = integration_level_t::UNDEFINED;
+        }
         return hardware_interface::CallbackReturn::SUCCESS;
     }
 
-    hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & /*previous_state*/) override
+    hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override
     {
-        RCLCPP_INFO(logger_, "Activating ...please wait...");
-
-        // 启动时命令和状态应相等
-        // for (const auto &[name, descr] : joint_state_interfaces_)
-        // {
-        //     set_command(name, get_state(name));
-        // }
-        // for (const auto &[name, descr] : gpio_command_interfaces_)
-        // {
-        //     set_command(name, get_state(name));
-        // }
-
-        RCLCPP_INFO(logger_, "Successfully activated!");
-
+        RCLCPP_DEBUG(get_logger(), "on_deactivate ...");
         return hardware_interface::CallbackReturn::SUCCESS;
     }
 
-    hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/) override
+    hardware_interface::return_type read(const rclcpp::Time &time, const rclcpp::Duration &period) override
     {
-        RCLCPP_INFO(logger_, "Successfully deactivated!");
-
-        return hardware_interface::CallbackReturn::SUCCESS;
-    }
-
-    // std::vector<hardware_interface::StateInterface> export_state_interfaces() override
-    // {
-    //     std::vector<hardware_interface::StateInterface> state_interfaces;
-
-    //     for (const auto &joint : info_.joints)
-    //     {
-    //         for (const auto &state_interface : joint.state_interfaces)
-    //         {
-    //             state_interfaces.emplace_back(
-    //                 hardware_interface::StateInterface(joint.name, state_interface.name, &joint_state_interfaces_[joint.name + "/" + state_interface.name]));
-    //         }
-    //     }
-
-    //     for (const auto &gpio : info_.gpios)
-    //     {
-    //         for (const auto &state_interface : gpio.state_interfaces)
-    //         {
-    //             state_interfaces.emplace_back(hardware_interface::StateInterface(gpio.name, state_interface.name, &gpio_state_interfaces_[gpio.name + "/" + state_interface.name]));
-    //         }
-    //     }
-
-    //     return state_interfaces;
-    // }
-
-    // std::vector<hardware_interface::CommandInterface> export_command_interfaces() override
-    // {
-    //     std::vector<hardware_interface::CommandInterface> command_interfaces;
-
-    //     for (const auto &joint : info_.joints)
-    //     {
-    //         for (const auto &command_interface : joint.command_interfaces)
-    //         {
-    //             command_interfaces.emplace_back(
-    //                 hardware_interface::CommandInterface(joint.name, command_interface.name, &joint_command_interfaces_[joint.name + "/" + command_interface.name]));
-    //         }
-    //     }
-
-    //     for (const auto &gpio : info_.gpios)
-    //     {
-    //         for (const auto &command_interface : gpio.command_interfaces)
-    //         {
-    //             command_interfaces.emplace_back(
-    //                 hardware_interface::CommandInterface(gpio.name, command_interface.name, &gpio_command_interfaces_[gpio.name + "/" + command_interface.name]));
-    //         }
-    //     }
-
-    //     return command_interfaces;
-    // }
-
-    hardware_interface::return_type read(const rclcpp::Time & /*time*/, const rclcpp::Duration &period) override
-    {
-        std::stringstream ss;
-        ss << "Reading states:";
-
-        // 模拟关节运动
-        for (const auto &joint : info_.joints)
+        for (std::size_t i = 0; i < hw_states_positions_.size(); i++)
         {
-            const std::string pos_state_name = joint.name + "/" + hardware_interface::HW_IF_POSITION;
-            const std::string vel_state_name = joint.name + "/" + hardware_interface::HW_IF_VELOCITY;
-            const std::string pos_cmd_name = joint.name + "/" + hardware_interface::HW_IF_POSITION;
+            //     switch (control_level_[i])
+            //     {
+            //     case integration_level_t::UNDEFINED:
+            //         break;
+            //     case integration_level_t::POSITION:
+            //         hw_states_velocities_[i] = 0;
+            //         hw_states_positions_[i] += (hw_commands_positions_[i] - hw_states_positions_[i]) / hw_slowdown_;
+            //         break;
+            //     case integration_level_t::VELOCITY:
+            //         hw_states_positions_[i] += (hw_states_velocities_[i] * period.seconds()) / hw_slowdown_;
+            //         break;
+            //     default:
+            //         break;
 
-            // 更新位置状态
-            double new_pos = get_state(pos_state_name) + (get_command(pos_cmd_name) - get_state(pos_state_name)) / hw_slowdown_;
-            set_state(pos_state_name, new_pos);
-
-            // 计算并更新速度状态
-            double velocity = (get_command(pos_cmd_name) - get_state(pos_state_name)) / period.seconds();
-            set_state(vel_state_name, velocity);
-
-            ss << std::fixed << std::setprecision(2) << std::endl << "\t" << get_state(pos_state_name) << " for joint '" << joint.name << "'";
+            RCLCPP_DEBUG(get_logger(), "read. [%d] control_level: %d, pos state: %.2f, pos command: %.2f...", i, control_level_[i], hw_states_positions_[i],
+                         hw_commands_positions_[i]);
         }
-
-        // 模拟GPIO输入
-        // 模拟数字输入
-        unsigned int seed = static_cast<unsigned int>(time(NULL)) + 1;
-        const std::string digital_input_name = info_.gpios[0].name + "/" + info_.gpios[0].state_interfaces[0].name;
-        set_state(digital_input_name, static_cast<double>(rand_r(&seed) % 2));
-
-        // 模拟模拟输入
-        seed = static_cast<unsigned int>(time(NULL)) + 2;
-        const std::string analog_input_name = info_.gpios[0].name + "/" + info_.gpios[0].state_interfaces[1].name;
-        set_state(analog_input_name, static_cast<double>(rand_r(&seed) % 1000) / 1000.0);
-
-        // 回读GPIO输出作为输入
-        for (size_t i = 0; i < info_.gpios[1].state_interfaces.size(); i++)
-        {
-            const std::string state_name = info_.gpios[1].name + "/" + info_.gpios[1].state_interfaces[i].name;
-            const std::string cmd_name = info_.gpios[1].name + "/" + info_.gpios[1].command_interfaces[i].name;
-            set_state(state_name, get_command(cmd_name));
-
-            ss << std::fixed << std::setprecision(2) << std::endl << "\t" << get_state(state_name) << " from GPIO input '" << state_name << "'";
-        }
-
-        RCLCPP_INFO_STREAM_THROTTLE(logger_, *get_clock(), 500, ss.str());
 
         return hardware_interface::return_type::OK;
     }
 
-    hardware_interface::return_type write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) override
+    hardware_interface::return_type write(const rclcpp::Time &time, const rclcpp::Duration &period) override
     {
-        std::stringstream ss;
-        ss << "Writing commands:";
-
-        // 模拟发送命令到硬件
-        for (const auto &[name, descr] : gpio_command_interfaces_)
+        for (std::size_t i = 0; i < hw_commands_positions_.size(); i++)
         {
-            // 模拟发送命令到硬件
-            ss << std::fixed << std::setprecision(2) << std::endl << "\t" << get_command(name) << " for GPIO output '" << name << "'";
+            RCLCPP_DEBUG(get_logger(), "write. [%d] control_level: %d, pos state: %.2f, pos command: %.2f...", i, control_level_[i], hw_states_positions_[i],
+                         hw_commands_positions_[i]);
         }
-
-        for (const auto &[name, descr] : joint_command_interfaces_)
-        {
-            ss << std::fixed << std::setprecision(2) << std::endl << "\t" << get_command(name) << " for joint '" << name.substr(0, name.find("/")) << "'";
-        }
-
-        RCLCPP_INFO_STREAM_THROTTLE(logger_, *get_clock(), 500, ss.str());
-
         return hardware_interface::return_type::OK;
+    }
+
+    /// Get the logger of the SystemInterface.
+    /**
+     * \return logger of the SystemInterface.
+     */
+    rclcpp::Logger get_logger() const
+    {
+        return *logger_;
+    }
+
+    /// Get the clock of the SystemInterface.
+    /**
+     * \return clock of the SystemInterface.
+     */
+    rclcpp::Clock::SharedPtr get_clock() const
+    {
+        return clock_;
     }
 
   private:
-    // 硬件减速因子
-    double hw_slowdown_ = 50.0; // 模拟硬件响应延迟
-    rclcpp::Logger logger_ = rclcpp::get_logger("MyHardwareController");
-};
+    // Objects for logging
+    std::shared_ptr<rclcpp::Logger> logger_;
+    rclcpp::Clock::SharedPtr clock_;
 
+    // Store the commands for the simulated robot
+    std::vector<double> hw_commands_positions_;
+    std::vector<double> hw_commands_velocities_;
+    std::vector<double> hw_states_positions_;
+    std::vector<double> hw_states_velocities_;
+
+    // Enum defining at which control level we are
+    // Dumb way of maintaining the command_interface type per joint.
+    enum integration_level_t : std::uint8_t
+    {
+        UNDEFINED = 0,
+        POSITION = 1,
+        VELOCITY = 2,
+        ACCELERATION = 3
+    };
+
+    // Active control mode for each actuator
+    std::vector<integration_level_t> control_level_;
+};
 } // namespace my_robot_urdf
 
 #include "pluginlib/class_list_macros.hpp"
